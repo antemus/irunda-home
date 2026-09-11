5d0de08e5fd5c283962d102c0a6348e5026df3c7
irunda <irunda@irunda.co.kr>
2026-09-12 07:34:42 +0900

    Support sitemap access at /public/sitemap.xml via rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/public/sitemap.xml',
+        destination: '/sitemap.xml',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
